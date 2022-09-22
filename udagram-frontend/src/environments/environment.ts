afd962e73bb23b420a28526f12424fbb8722132f
DudeGFA <favouridowuagida0@gmail.com>
2022-09-22 23:18:03 +0100

    update udagram-frontend\src\environments\environment.ts apihost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af911d6514db647238af542733c93394-1183961744.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
